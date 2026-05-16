18c7d2215e9a0237918ee3879dfc38b1bda5a9fb
Shivam Kapoor <kapoorshivam7178@gmail.com>
2026-05-17 02:48:03 +0530

    Lanchain Runnables: Passthrough $ Lambda Runnables

---
+++ Langchain_Runnables/runable_passthrough.py
@@ -0,0 +1,34 @@
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
+
+load_dotenv()
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = GoogleGenerativeAI(model='gemini-2.5-flash', temperature=0.7)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(), # runnable passthrough to get info inbetween a runnables -> it allows you to pass the output of one runnable to another runnable without modification
+    'word_count': RunnableLambda(word_count) # runnable lambda to calculate the word count of the joke using the word_count function -> it created any python function a runnable
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
